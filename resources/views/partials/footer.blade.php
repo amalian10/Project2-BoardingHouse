97ed2c36e6774476ef7528ec6093cb2af6aa6cda
Amalia Nuraini <2041720160@student.polinema.ac.id>
2022-12-16 15:17:23 +0700

    Tampilan

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,60 @@
+<style type="text/css">
+.list-unstyled p{
+    color: black;
+}
+</style>
+<section class="h-100 w-100" id="footer" style="box-sizing: border-box; background-color:#ebd5d5;">
+<div class="footer-2-2 container-xxl mx-auto position-relative p-0" style="font-family: 'Poppins', sans-serif">
+    <div class="list-footer">
+        <div class="row gap-md-0 gap-3">
+            <div class="col-lg-4 col-md-6">
+                <h2 class="footer-text-title">Media Sosial</h2>
+                <nav class="list-unstyled">
+                    <li class="list-space">
+                        <p>Instagram : @boardinghouse</p>
+                    </li>
+                    <li class="list-space">
+                        <p>Facebook  : @boardinghouse</p>
+                    </li>
+                </nav>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h2 class="footer-text-title">Hubungi</h2>
+                <nav class="list-unstyled">
+                    <li class="list-space">
+                        <p>081282929977</p>
+                    </li>
+                    <li class="list-space">
+                        <p>boardinghouse@gmail.com</p>
+                    </li>
+                </nav>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h2 class="footer-text-title">Lokasi</h2>
+                <nav class="list-unstyled">
+                    <li class="list-space">
+                        <p> Semanggi Barat No. 07</p>
+                    </li>
+                    <li class="list-space">
+                        <p>Kota Malang</p>
+                    </li>
+                    <li class="list-space">
+                        <p>Jawa Timur</p>
+                    </li>
+                </nav>
+            </div>
+        </div>
+        <div class="border-color info-footer">
+            <div class="">
+                <hr class="hr" />
+            </div>
+            <div class="mx-auto d-flex flex-column flex-lg-row align-items-center footer-info-space gap-4">
+                <nav class="d-flex flex-lg-row flex-column align-items-center justify-content-center">
+                    <p style="margin: 0">Copyright © 2022 Boarding House</p>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+</div>
+</section>
